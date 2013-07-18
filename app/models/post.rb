105225ba8cd97f7e52beff872430ba5084fe5938
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-18 16:59:56 -0500

    created and migrated tables
    beginning testing of database
    beginning creation of routes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   has_many :tags , through: :posts_tags
   has_many :posts_tags
 
-  :title presencce
+  validates :title , presence: true , :body , presence: true
 end
